16685d752c6f3bfb10967793f98886354f25fcc4
Spencer Richards <spencer.richards7@gmail.com>
2017-11-18 14:05:26 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Heather Olsen', email: 'test@test.com', password: 'password', password_confirmation: 'password')
+puts 'Test User Created!'
+
+5.times do 
+  ArtWork.create(title: 'Test Name', url: 'http://res.cloudinary.com/dkrn2wmhn/image/upload/v1511038352/hoa_drawing_tq2vga.jpg',
+                  type_of: 'drawing', medium: 'charcoal', surface: 'paper', dimensions: '20 x 20', price: 100.00, date_complete: Time.now)
+end
+puts 'Drawings seeded!'
+
+5.times do 
+  ArtWork.create(title: 'Test Name', url: 'http://res.cloudinary.com/dkrn2wmhn/image/upload/v1511038369/8x10_300_dpi_jtgek1.jpg',
+                  type_of: 'comission', medium: 'oil', surface: 'panel', dimensions: '20 x 30', price: 340.00, date_complete: Time.now)
+end
+puts 'Comissions seeded!'
+
+5.times do 
+  ArtWork.create(title: 'Test Name', url: 'http://res.cloudinary.com/dkrn2wmhn/image/upload/v1511038401/10x10_300_dpi_lk5spb.jpg',
+                  type_of: 'painting', medium: 'oil', surface: 'canvas', dimensions: '25 x 20', price: 250.00, date_complete: Time.now)
+end
+puts 'Paintings seeded!'
