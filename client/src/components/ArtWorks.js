80862eab5d0fe7026ec5a5c0c1a3acdebf7afc81
Spencer Richards <spencer.richards7@gmail.com>
2017-11-20 18:26:56 -0700

    add transition effect

--- client/src/components/ArtWorks.js
+++ client/src/components/ArtWorks.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Grid, Header, Image } from 'semantic-ui-react';
+import { Container, Grid, Header, Image, Transition } from 'semantic-ui-react';
 
 class ArtWorks extends React.Component {
+  state = { visible: false };
 
   componentDidMount() {
     const { fetchArtWorks, dispatch } = this.props;
     dispatch(fetchArtWorks());
+    this.setState({visible: !this.state.visible})
   }
 
   componentWillReceiveProps(nextProps){
@@ -18,7 +20,9 @@ class ArtWorks extends React.Component {
   displayArtWorks = () => {
     return this.props.works.map(comission =>
       <Grid.Column width={4} key={comission.id}>
-        <Image src={comission.url} fluid />
+        <Transition visible={this.state.visible} animation='fly right' duration={1000}>
+          <Image src={comission.url} fluid />
+        </Transition>
       </Grid.Column>
     )
   }
