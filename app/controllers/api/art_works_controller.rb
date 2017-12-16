107f49524b1cefa266b1378dcce00a3d9c7a5fc8
Spencer Richards <spencer.richards7@gmail.com>
2017-12-16 11:28:11 -0700

    cloudinary auth added to yml

--- app/controllers/api/art_works_controller.rb
+++ app/controllers/api/art_works_controller.rb
@@ -13,18 +13,24 @@ class Api::ArtWorksController < ApplicationController
   end
 
   def create
-    auth = {
-      cloud_name: ENV['CLOUD_NAME'],
-      api_key: ENV['API_KEY'],
-      api_secret: ENV['API_SECRET']
-    }
+    # auth = {
+    #   cloud_name: ENV['CLOUD_NAME'],
+    #   api_key: ENV['API_KEY'],
+    #   api_secret: ENV['API_SECRET']
+    # }
 
     uploaded_image_name = params.keys.first
     uploaded_file = params[uploaded_image_name]
+        
     begin
-      cloud_image = Cloudinary::Uploader.upload(uploaded_file, auth)
+      puts '1st HIT!'
+      cloud_image = Cloudinary::Uploader.upload(uploaded_file)
+      puts '2nd HIT!'
+      # binding.pry
+      # Cloudinary::Uploader.upload(cloud_image, :public_id => params[:title])
       # TODO: Can I utilize art_works_params?
       art_work = ArtWork.create(url: cloud_image['url'], title: params['title'], type_of: params['type_of'], medium: params['medium'], surface: params['surface'], dimensions: params['dimensions'], price: params['price'], date_complete: params['date_complete'])
+      puts '3rd HIT!'
       render json: art_work
     rescue
       # TODO: Generate an error 
@@ -35,6 +41,15 @@ class Api::ArtWorksController < ApplicationController
 
   def destroy
     @art_work = ArtWork.find(params[:id])
+
+    # auth = {
+    #   cloud_name: ENV['CLOUD_NAME'],
+    #   api_key: ENV['API_KEY'],
+    #   api_secret: ENV['API_SECRET']
+    # }
+    # binding.pry
+    # Cloudinary::Api.delete_resources(['image1'])
+
     @art_work.destroy
   end
 
