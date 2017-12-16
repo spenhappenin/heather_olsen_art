2ab32d5c0ecd31cfbf77cd283c2e87e714b7b413
Spencer Richards <spencer.richards7@gmail.com>
2017-12-16 12:26:34 -0700

    delete cloudinary image on delete

--- app/controllers/api/art_works_controller.rb
+++ app/controllers/api/art_works_controller.rb
@@ -13,43 +13,23 @@ class Api::ArtWorksController < ApplicationController
   end
 
   def create
-    # auth = {
-    #   cloud_name: ENV['CLOUD_NAME'],
-    #   api_key: ENV['API_KEY'],
-    #   api_secret: ENV['API_SECRET']
-    # }
-
     uploaded_image_name = params.keys.first
     uploaded_file = params[uploaded_image_name]
-        
+
     begin
-      puts '1st HIT!'
-      cloud_image = Cloudinary::Uploader.upload(uploaded_file)
-      puts '2nd HIT!'
-      # binding.pry
-      # Cloudinary::Uploader.upload(cloud_image, :public_id => params[:title])
+      cloud_image = Cloudinary::Uploader.upload(uploaded_file, public_id: params[:title])
       # TODO: Can I utilize art_works_params?
       art_work = ArtWork.create(url: cloud_image['url'], title: params['title'], type_of: params['type_of'], medium: params['medium'], surface: params['surface'], dimensions: params['dimensions'], price: params['price'], date_complete: params['date_complete'])
-      puts '3rd HIT!'
       render json: art_work
     rescue
       # TODO: Generate an error 
       puts 'Some error...'
-      # render json: { errors: e }, status: :bad_request
     end
   end
 
   def destroy
     @art_work = ArtWork.find(params[:id])
-
-    # auth = {
-    #   cloud_name: ENV['CLOUD_NAME'],
-    #   api_key: ENV['API_KEY'],
-    #   api_secret: ENV['API_SECRET']
-    # }
-    # binding.pry
-    # Cloudinary::Api.delete_resources(['image1'])
-
+    Cloudinary::Api.delete_resources([@art_work.title])
     @art_work.destroy
   end
 
