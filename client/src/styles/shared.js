6b975a5515e71fa4c481f2b9f7ff7dd8670b69a4
Spencer Richards <spencer.richards7@gmail.com>
2018-01-04 10:59:50 -0700

    add top and bottom margin to tablet and smaller

--- client/src/styles/shared.js
+++ client/src/styles/shared.js
@@ -24,9 +24,9 @@ export const media = Object.keys(sizes).reduce((accumulator, label) => {
 export const StyledContainer = styled(Segment)`
   ${ media.giant`padding: 50px 300px 100px 300px !important;`}
   ${ media.desktop`padding: 50px 160px 100px 160px !important;`}
-  ${ media.tablet`padding: 0 50px !important;`}
-  ${ media.phablet`padding: 0 30px !important;`}
-  ${ media.phone`padding: 0 20px !important;` }
+  ${ media.tablet`padding: 50px 50px 100px 50px !important;`}
+  ${ media.phablet`padding: 50px 30px 100px 30px !important;`}
+  ${ media.phone`padding: 50px 20px 100px 20px !important;` }
 `
 
 export const StyledHeader = styled(Header)`
