5ca8f50f2e7efda261cd1fbd48641adb627ab6af
Spencer Richards <spencer.richards7@gmail.com>
2018-01-04 11:45:16 -0700

    styled component copyright

--- client/src/components/Copyright.js
+++ client/src/components/Copyright.js
@@ -1,11 +1,9 @@
 import React from 'react';
+import { CopyrightSegment } from '../styles/copyright';
 import { Segment } from 'semantic-ui-react';
 
 const Copyright = () => (
-  <Segment basic>
-    <br />
-    <br />
-    <br />
+  <Segment as={CopyrightSegment} basic>
     <p>All images and content of this website are copyrighted by the artist, Heather Olsen. Any use or reproduction in any form without permission is prohibited.</p>
   </Segment>
 )
---
+++ client/src/styles/copyright.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Segment } from 'semantic-ui-react';
+
+export const CopyrightSegment = styled(Segment) `
+  margin-top: 35px !important;
+`
