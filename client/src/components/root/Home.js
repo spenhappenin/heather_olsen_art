d6b0e999aa18db2f867ef5742b65dcd0161ea222
Spencer Richards <spencer.richards7@gmail.com>
2018-02-06 12:55:03 -0700

    add redesign modal

--- client/src/components/root/Home.js
+++ client/src/components/root/Home.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 
 class Home extends Component {
+  state = { modal: true };
+
+  closeModal = () => {
+    const { modal } = this.state;
+    this.setState({ modal: false });
+  }
+
   render() {
     return(
-      <StyledBackground>
-      </StyledBackground>
+      <div>
+        <Modal defaultOpen={true} open={this.state.modal} basic size='small'>
+          <Header as='h1' icon='smile' content='We are currently redesigning BUT we are open!' />
+          <Modal.Content>
+            <Header as='h3' inverted>Please feel free to checkout all the artwork and the website should be complete as soon as possible. We apologize for the inconvenience! </Header>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button size='huge' color='blue' inverted onClick={this.closeModal}>
+              <Icon name='thumbs outline up' /> Okay
+            </Button>
+          </Modal.Actions>
+        </Modal>
+        <StyledBackground></StyledBackground>
+      </div>
     );
   }
 }
