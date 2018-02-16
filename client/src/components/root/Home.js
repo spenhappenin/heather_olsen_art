3f6b466472a382194ea3f553441088a31259575e
Spencer Richards <spencer.richards7@gmail.com>
2018-02-15 20:54:48 -0700

    remove modal from home component

--- client/src/components/root/Home.js
+++ client/src/components/root/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { StyledBackground } from '../../styles/home';
-import { Button } from '../../styles/shared';
-import { Header, Modal, Transition } from 'semantic-ui-react';
+import { Transition } from 'semantic-ui-react';
 
 class Home extends Component {
   state = { modal: true, visible: false };
@@ -16,19 +15,8 @@ class Home extends Component {
 
   render() {
     return(
-      <div>
-        <Modal open={this.state.modal} basic size='small'>
-          <Header as='h1' icon='smile' content='We are currently redesigning BUT we are open!' />
-          <Modal.Content>
-            <Header as='h3' inverted>Please feel free to checkout all the artwork and the website should be complete as soon as possible. We apologize for the inconvenience! </Header>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button modal onClick={this.closeModal}>Okay</Button>
-          </Modal.Actions>
-        </Modal>
-        <Transition visible={this.state.visible} animation='fade' duration={2500}>
-          <StyledBackground></StyledBackground>
-        </Transition>
+      <div visible={this.state.visible} animation='fade' duration={2500}>
+          <StyledBackground></StyledBackground>>
       </div>
     );
   }
