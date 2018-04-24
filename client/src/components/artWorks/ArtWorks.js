0e8c501419743157a4998441d1e6df7be0ede9c2
Spencer Richards <spencer.richards7@gmail.com>
2018-04-24 13:22:14 -0600

    remove image on error

--- client/src/components/artWorks/ArtWorks.js
+++ client/src/components/artWorks/ArtWorks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ArtWork from './ArtWork';
 import Copyright from '../shared/Copyright';
 import InfiniteScroll from "react-infinite-scroller";
 import Lightbox from 'react-images';
@@ -8,7 +9,7 @@ import { DimmerContainer, StyledContainer, } from '../../styles/shared';
 import { Dimmer, Grid, Image, Loader, Segment, Transition, } from 'semantic-ui-react';
 
 class ArtWorks extends React.Component {
-  state = { currentImage: 0, lightboxIsOpen: false, loaded: false };
+  state = { currentImage: 0, erroredImages: [], lightboxIsOpen: false, loaded: false };
 
   componentDidMount() {
     const { fetchArtWorks, dispatch } = this.props;
@@ -27,24 +28,32 @@ class ArtWorks extends React.Component {
       lightboxIsOpen: false,
     });
   };
+
+  handleImageError = (id) => this.setState({ erroredImages: [...this.state.erroredImages, id] });
   
   displayArtWorks = () => {
-    const { works } = this.props;
-    if(!works) return;
-    return works.map( (artWork, i) =>
-      <Grid.Column key={artWork.id} mobile={8} tablet={4} computer={4} style={styles.flex}>
-        <Transition visible={this.state.visible} animation='fade' duration={2000}>
-            <Image 
+    const { artWorks } = this.props;
+    const { erroredImages, visible } = this.state;
+    
+    if(!artWorks) return;
+
+    return artWorks.map( (artWork, i) => {
+      return erroredImages.includes(artWork.id) ? 
+        null
+      :
+        <Grid.Column key={artWork.id} mobile={8} tablet={4} computer={4} style={styles.flex}>
+          <Transition visible={visible} animation='fade' duration={2000}>
+            <Image
               alt={artWork.title}
-              src={artWork.src} 
-              href={artWork.src} 
-              onClick={(e) => this.openLightbox(i, e)} 
-              onError={(e) => { e.target.src ="https://res.cloudinary.com/dtb6lx1s4/image/upload/v1518813497/ImageNotAvailable_owzy6a.png" }}
-              fluid 
-              />
-        </Transition>
-      </Grid.Column>
-    )
+              src={artWork.src}
+              href={artWork.src}
+              onClick={(e) => this.openLightbox(i, e)}
+              onError={() => this.handleImageError(artWork.id)}
+              fluid
+            />
+          </Transition>
+        </Grid.Column>
+    });
   };
 
   gotoImage = (index) => {
@@ -66,7 +75,9 @@ class ArtWorks extends React.Component {
   };
   
   handleClickImage = () => {
-    if(this.state.currentImage === this.props.works.length - 1) return;
+    const { artWorks } = this.props;
+    
+    if(this.state.currentImage === artWorks.length - 1) return;
     this.gotoNext();
   };
 
@@ -86,9 +97,8 @@ class ArtWorks extends React.Component {
 
   setLoaded = () => this.setState({ loaded: true });
 
-
   render() {
-    const { currentPage, totalPages } = this.props;
+    const { artWorks, currentPage, totalPages } = this.props;
 
     if(this.state.loaded) {
       return (
@@ -105,7 +115,7 @@ class ArtWorks extends React.Component {
               {this.displayArtWorks()}
               <Lightbox
                 currentImage={this.state.currentImage}
-                images={this.props.works}
+                images={artWorks}
                 isOpen={this.state.lightboxIsOpen}
                 onClickImage={this.handleClickImage}
                 onClickNext={this.gotoNext}
@@ -133,11 +143,11 @@ class ArtWorks extends React.Component {
 const mapStateToProps = (state, props) => {
   switch (props.type) {
     case 'comission':
-    return { works: state.comissions, totalPages: state.totalPages, currentPage: state.currentPage }
+    return { artWorks: state.comissions, totalPages: state.totalPages, currentPage: state.currentPage }
     case 'painting':
-    return { works: state.paintings, totalPages: state.totalPages, currentPage: state.currentPage }
+    return { artWorks: state.paintings, totalPages: state.totalPages, currentPage: state.currentPage }
     case 'drawing':
-    return { works: state.drawings, totalPages: state.totalPages, currentPage: state.currentPage }
+    return { artWorks: state.drawings, totalPages: state.totalPages, currentPage: state.currentPage }
     default:
     return {};
   }
