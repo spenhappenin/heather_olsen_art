895fe84832edb7cd7b6d94060ce75d7bf5829a31
Spencer Richards <spencer.richards7@gmail.com>
2018-07-31 11:37:28 -0600

    add type to form button on cv

--- client/src/components/admin/CvEditForm.js
+++ client/src/components/admin/CvEditForm.js
@@ -25,8 +25,8 @@ class CvEditForm extends React.Component {
 
   renderButtons = () => (
     <span>
-      <CvButton onClick={this.handleSubmit}>Accept</CvButton>
-      <CvButton onClick={this.props.toggleEdit}>Cancel</CvButton>
+      <CvButton onClick={this.handleSubmit} type='button'>Accept</CvButton>
+      <CvButton onClick={this.props.toggleEdit} type='button'>Cancel</CvButton>
     </span>
   );
  
@@ -92,25 +92,6 @@ class CvEditForm extends React.Component {
   };
 };
 
-const Form = styled.form`
-  display: flex;
-`;
-
-const Input = styled.input`
-  border: 1px solid rgba(34,36,38,.15);
-  box-shadow: 0 0 0 0 transparent inset;
-  color: rgba(0,0,0,.87);
-  font-family: Lato,'Helvetica Neue',Arial,Helvetica,sans-serif;
-  font-size: 14px !important;
-  height: 35px;
-  line-height: 1.21428571em;
-  margin: 0 0 5px 0;
-  outline: 0;
-  padding: .67857143em 1em;
-  transition: color .1s ease,border-color .1s ease;
-  width: ${ props => props.smaller ? '161px' : '225px' };
-`;
-
 const CvButton = styled.button`
   background-color: #272727;
   border-color: #272727;
@@ -131,6 +112,25 @@ const CvButton = styled.button`
     background-color: #595959;
     transition: background-color 0.3s ease;
   }
+`;
+
+const Form = styled.form`
+  display: flex;
+`;
+
+const Input = styled.input`
+  border: 1px solid rgba(34,36,38,.15);
+  box-shadow: 0 0 0 0 transparent inset;
+  color: rgba(0,0,0,.87);
+  font-family: Lato,'Helvetica Neue',Arial,Helvetica,sans-serif;
+  font-size: 14px !important;
+  height: 35px;
+  line-height: 1.21428571em;
+  margin: 0 0 5px 0;
+  outline: 0;
+  padding: .67857143em 1em;
+  transition: color .1s ease,border-color .1s ease;
+  width: ${ props => props.smaller ? '161px' : '225px' };
 `;
 
 export default connect()(CvEditForm);
