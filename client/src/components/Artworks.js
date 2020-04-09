7ab46001e0d4f9e0d5c328a9f20ead56f9cfd466
Spencer Richards <spencer.richards7@gmail.com>
2020-04-08 20:56:21 -0600

    test fix for mobile image stretch

--- client/src/components/Artworks.js
+++ client/src/components/Artworks.js
@@ -114,9 +114,9 @@ const Column = styled.div`
 `;
 
 const ImageContainer = styled.div`
-	position: relative;
+	/* position: relative;
 	display: flex;
-	justify-content: flex-end;
+	justify-content: flex-end; */
   width: 100%;
 `;
 
