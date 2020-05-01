e10c99e4e67bc36ddcf39b12b852c6cf71fef0d6
Spencer Richards <spencer.richards7@gmail.com>
2020-04-30 19:02:15 -0600

    start sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "job_tracker_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name =>     ENV['SENDGRID_USERNAME'],
+    :password =>      ENV['SENDGRID_PASSWORD'],
+    :domain =>        ENV['SENDGRID_DOMAIN'],
+    :address =>       "smtp.sendgrid.net",
+                      :port => 587,
+                      :authentication => :plain,
+                      :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { host: "https://heatherolsenart.com" }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
